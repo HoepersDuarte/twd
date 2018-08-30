9fab415a731b84efd4c85a16897c94e22645744e
Gabriel Duarte <hoepersduarte@gmail.com>
2018-08-30 11:31:11 -0300

    add templates routes and static files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-
-    texto = '''
-    <h1> Rango </h1>
-    Essa é a página principal do Rango
-    
-    <br/><a href='/rango/about/'>Sobre o rango</a>
-    '''
-    return HttpResponse(texto)
+    context_dict = {}
+    context_dict['boldmessage'] = 'Hellow Word!'
+    context_dict['autor'] = 'HoepersDuarte'
+    return render(request, 'rango/inde.html', context=context_dict)
 
 
 def about(request):
